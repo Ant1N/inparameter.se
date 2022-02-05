1e12506414b3caba55d40111f952f8658accfb4b
Isak Antin <isakantin@Isaks-MacBook-Air.local>
2022-02-05 12:06:02 +0100

    added contact page

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const tl2 = gsap.timeline({
     paused: "true",
     reversed: "true"
 })
+const pop = gsap.timeline({
+    paused: "true"
+})
 
 // tl1 animations 
 tl1.from(".logo, .menu", {
@@ -28,6 +31,14 @@ tl1.from(".container-row img", {
         amount: .4
     }
 },"-=1.2")
+tl1.from(".contact-page",{
+    duration: 1,
+    opacity: 0,
+    y:"-100%",
+    stagger: {
+        amount: 1
+    }
+});
 // tl2 animations 
 tl2.to(".container-row h1, .container-row h2", {
     duration: .3,
@@ -62,4 +73,27 @@ tl2.from(".menu-items li, .social-items li", {
 
 function menu(){
     tl2.reversed() ? tl2.play() : tl2.reverse();
+}
+
+pop.to(".popup-template", {
+    duration: .8,
+    opacity: 1,
+    y: 0,
+    scale: 1,
+    ease: Power4.easeOut
+})
+pop.from(".popup-top, .form-row1 h1, .form-row1 input, .form-row1 textarea, .submit-popup", {
+    duration: .5,
+    opacity: 0,
+    y: 15,
+    stagger: {
+        amount: .4
+    }
+},"-=.3")
+
+function open_pop(){
+    pop.play();
+}
+function close_pop(){
+    pop.reverse();
 }
